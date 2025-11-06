806bd54b589829f21b9547fe6a90981410af1e04
Ronny Dumancela <ronnydumancela1997@gmail.com>
2025-11-06 06:30:38 -0500

    We added the model for Person

--- Aplicaciones/Persona/models.py
+++ Aplicaciones/Persona/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Persona(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=150, blank=True)
+    telefono = models.CharField(max_length=20, blank=True)
+    correo = models.EmailField(blank=True)
+    foto_perfil = models.FileField(upload_to='media/personas/perfiles/', blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.nombres} {self.apellidos}"
