b05a08846fe4e33318d60c7a3ca03d928f9d1dd6
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-11-07 05:23:14 -0500

    Show all pets

--- Aplicaciones/Mascota/views.py
+++ Aplicaciones/Mascota/views.py
@@ -4,3 +4,8 @@ from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Mascota
+
+
+def lista_mascotas(request):
+    mascotas = Mascota.objects.all()
+    return render(request, 'Mascota/inicio.html', {'mascotas': mascotas})
