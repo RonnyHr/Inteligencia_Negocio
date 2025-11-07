3b79e7bda418cdc9418bc6f47e9fa7c20765b072
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-11-07 05:26:36 -0500

    Save new pet

--- Aplicaciones/Mascota/views.py
+++ Aplicaciones/Mascota/views.py
@@ -13,3 +13,26 @@ def lista_mascotas(request):
 
 def nueva_mascota(request):
     return render(request, 'Mascota/nuevaMascota.html')
+
+
+def guardar_mascota(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        especie = request.POST.get('especie')
+        raza = request.POST.get('raza')
+        edad = request.POST.get('edad')
+        fecha_nacimiento = request.POST.get('fecha_nacimiento') or None
+        descripcion = request.POST.get('descripcion')
+        foto_perfil = request.FILES.get('foto_perfil')
+
+        Mascota.objects.create(
+            nombre=nombre,
+            especie=especie,
+            raza=raza,
+            edad=edad,
+            fecha_nacimiento=fecha_nacimiento,
+            descripcion=descripcion,
+            foto_perfil=foto_perfil,
+        )
+        messages.success(request, 'Mascota registrada correctamente.')
+    return redirect('listaMascota')
