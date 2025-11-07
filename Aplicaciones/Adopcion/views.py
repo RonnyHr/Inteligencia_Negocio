44e3ea741ff0bf245dac719fac2f4dc1a7fa9b4f
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-11-07 05:44:01 -0500

    Show form to add a new adoption

--- Aplicaciones/Adopcion/views.py
+++ Aplicaciones/Adopcion/views.py
@@ -15,3 +15,12 @@ from .models import Adopcion
 def lista_adopciones(request):
     adopciones = Adopcion.objects.select_related('mascota', 'persona').all()
     return render(request, 'Adopcion/inicio.html', {'adopciones': adopciones})
+
+
+def nueva_adopcion(request):
+    mascotas_disponibles = Mascota.objects.filter(adopciones__isnull=True)
+    contexto = {
+        'mascotas': mascotas_disponibles,
+        'personas': Persona.objects.all(),
+    }
+    return render(request, 'Adopcion/agregarAdopcion.html', contexto)
