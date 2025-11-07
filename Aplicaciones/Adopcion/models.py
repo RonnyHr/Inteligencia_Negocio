b46373a3e8c9f6c357a320121e6ce065d4182191
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-11-07 05:37:48 -0500

    We added the model for the adoption entity

--- Aplicaciones/Adopcion/models.py
+++ Aplicaciones/Adopcion/models.py
@@ -1,3 +1,20 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
-# Create your models here.
+
+class Adopcion(models.Model):
+    mascota = models.ForeignKey('Mascota.Mascota', on_delete=models.CASCADE, related_name='adopciones')
+    persona = models.ForeignKey('Persona.Persona', on_delete=models.CASCADE, related_name='solicitudes_adopcion')
+    fecha_solicitud = models.DateField(auto_now_add=True)
+    estado = models.CharField(max_length=50, default='pendiente')
+    observaciones = models.TextField(blank=True)
+    documento_pdf = models.FileField(
+        upload_to='media/adopciones/documentos/',
+        blank=True,
+        null=True,
+        validators=[FileExtensionValidator(['pdf'])],
+    )
+
+    def __str__(self) -> str:
+        return f"{self.persona} - {self.mascota} ({self.estado})"
+
