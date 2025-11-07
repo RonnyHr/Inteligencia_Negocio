c5f52c72bfa4e977e16cebcd7e74dd47d7c8f352
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-11-07 05:21:41 -0500

    We added the imports for the pet entity

--- Aplicaciones/Persona/models.py
+++ Aplicaciones/Persona/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
-
 class Persona(models.Model):
     nombres = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
