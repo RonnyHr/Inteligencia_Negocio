e70f301e15c352c2b81631102178274487f8d50c
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-11-07 05:45:24 -0500

    Save new adoption request

--- Aplicaciones/Adopcion/views.py
+++ Aplicaciones/Adopcion/views.py
@@ -24,3 +24,15 @@ def nueva_adopcion(request):
         'personas': Persona.objects.all(),
     }
     return render(request, 'Adopcion/agregarAdopcion.html', contexto)
+
+
+def guardar_adopcion(request):
+    if request.method == 'POST':
+        mascota_id = request.POST.get('mascota')
+        persona_id = request.POST.get('persona')
+        estado = request.POST.get('estado')
+        observaciones = request.POST.get('observaciones')
+        documento_pdf = request.FILES.get('documento_pdf')
+
+        mascota = get_object_or_404(Mascota, pk=mascota_id)
+        persona = get_object_or_404(Persona, pk=persona_id)
