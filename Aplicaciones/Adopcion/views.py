31261a290b2db9223dc2d2c48b68deadf25bf9e9
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-11-07 05:41:38 -0500

    We added the imports for the adoption entity

--- Aplicaciones/Adopcion/views.py
+++ Aplicaciones/Adopcion/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import json
+import os
 
-# Create your views here.
+from django.contrib import messages
+from django.db.models import Count, Q
+from django.db.models.functions import TruncMonth
+from django.shortcuts import get_object_or_404, redirect, render
+
+from Aplicaciones.Mascota.models import Mascota
+from Aplicaciones.Persona.models import Persona
+
+from .models import Adopcion
