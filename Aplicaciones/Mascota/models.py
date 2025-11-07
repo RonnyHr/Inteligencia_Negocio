18d80881455d598bd231ddde6e7bad2adbac889d
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-11-07 05:15:37 -0500

    We created the model for the pet entity.

--- Aplicaciones/Mascota/models.py
+++ Aplicaciones/Mascota/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=100)
+    especie = models.CharField(max_length=50)
+    raza = models.CharField(max_length=50, blank=True)
+    edad = models.PositiveIntegerField(help_text="Edad aproximada en años")
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    descripcion = models.TextField(blank=True)
+    foto_perfil = models.FileField(upload_to='media/mascotas/perfiles/', blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.nombre
